f99cf09ed61bd53d3c0157bd2fb8136239038002
Brooks-portal <medtracker04@gmail.com>
2021-05-27 13:52:33 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var str = "hello developers!";
+console.log(str);
+
+var studentName;
+var studentName = "placeholder";
+var studentAge = 17;
+
+studentName = "Brook here";
+studentAge = 17;
+
+console.log(studentName);
+console.log(studentAge);
+console.log("this website is in early development stages");
+console.log("more things to come");
